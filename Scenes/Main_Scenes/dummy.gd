fae6ee4782e2f275fee33cf364449528ff077973
Thomas <Jack.t.lyons@outlook.com>
2025-12-23 13:21:14 -0500

    Took away dummy particles

--- Scenes/Main_Scenes/dummy.gd
+++ Scenes/Main_Scenes/dummy.gd
@@ -3,10 +3,8 @@ extends RigidBody3D
 var health = 100
 
 #Declare Nodes
-@onready var dummy_hit_particles = $Dummy_Hit_Particles
 @onready var dummy_hit_sound = $Dummy_Hit_Sound
 
 func damage_taken(damage):
 	health -= damage
-	dummy_hit_particles.emitting = true
 	dummy_hit_sound.playing = true
