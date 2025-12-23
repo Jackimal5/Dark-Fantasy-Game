956c220ec633d69e759468b5dfa71457b56ac004
Thomas <Jack.t.lyons@outlook.com>
2025-12-23 13:16:38 -0500

    Implemented Dummy SFX
    This added the dummy getting hit sound when you attack the dummy

--- Scenes/Main_Scenes/dummy.gd
+++ Scenes/Main_Scenes/dummy.gd
@@ -4,7 +4,9 @@ var health = 100
 
 #Declare Nodes
 @onready var dummy_hit_particles = $Dummy_Hit_Particles
+@onready var dummy_hit_sound = $Dummy_Hit_Sound
 
 func damage_taken(damage):
 	health -= damage
 	dummy_hit_particles.emitting = true
+	dummy_hit_sound.playing = true
